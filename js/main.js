04eecd580dfcd255be00fe1b568b44b488af6420
desmond700 <dwdeveloper4@gmail.com>
2018-05-05 21:17:39 -0400

    Removed unused code

--- js/main.js
+++ js/main.js
@@ -1,45 +1,47 @@
 
-$(function(){
+// Create web workers
+var viewedWorker = new Worker("js/addViewedItem_WW.js");
+var ajaxWorker = new Worker("js/Ajax_WW.js");
+var cartWorker = new Worker("js/AddToCart_WW.js");
+var getViewedWorker = new Worker("js/GetViewedItem_WW.js");
+var shoppingCrtWorker = new Worker("js/shoppingCrt_WW.js");
 
-  // Create web workers
-  var worker = new Worker("js/webWorker.js");
+$(function(){
 
   var jsonObj;
   var genre = "";
   var hashVal = window.location.hash.substring(1);
 
-  fetch("data/books.json").then((response) => {
-      response.json().then((json) => {
-        $(".se-pre-con").stop().fadeOut(600);
-        jsonObj = json;
-        window.sessionStorage.setItem("json", JSON.stringify(json));
+  ajaxWorker.onmessage = function(event){
+      $(".se-pre-con").stop().fadeOut(600);
+      jsonObj = event.data.Json;
+      $("i.itemCnt,span.itemCnt").html(event.data.recordCount);
+      $.each(jsonObj.Books, function (index, element) {
+          var img = $("<img />").attr("style", "height:248px")
+                                .attr("class", "img-thumbnail")
+                                .attr("src", "images/bookcover/" + element.img);
+          var anchorImg = $("<a />").attr("href", "./book?title="+element.Title).append(img)
+                                                     .attr("class", "ml-auto mr-auto");
+          var title = $("<p class='txt'></p>").append(element.Title);
+          var author = $("<h6 class='txtg'></h6>").append(element.Author);
+          var anchorTitle = $("<a />").attr("href", "./book?title="+element.Title).append(title);
+          var bookType = $("<p class='txtg'></p>").append(element["Book Type"]);
+          var price = $("<p class='txtg text-danger'></p>").append("CDN$ "+element.Price);
+          var parentDiv = $("<div class='viewedItem d-flex flex-column col-10 col-sm-5 col-md-3 col-lg-2 mx-3 mt-3 pt-3 bg-light' data-viewed-item='"+element.Title+"'></div>")
+                          .append(anchorImg, anchorTitle, author, bookType, price);
 
-        $.each(json.Books, function (index, element) {
-            var img = $("<img />").attr("style", "height:248px")
-                                  .attr("class", "img-thumbnail")
-                                  .attr("src", "images/bookcover/" + element.img);
-            var anchorImg = $("<a />").attr("href", "./book?title="+element.Title).append(img)
-                                                       .attr("class", "ml-auto mr-auto");
-            var title = $("<p class='txt'></p>").append(element.Title);
-            var author = $("<h6 class='txtg'></h6>").append(element.Author);
-            var anchorTitle = $("<a />").attr("href", "./book?title="+element.Title).append(title);
-            var bookType = $("<p class='txtg'></p>").append(element["Book Type"]);
-            var price = $("<p class='txtg text-danger'></p>").append("CDN$ "+element.Price);
-            var parentDiv = $("<div class='viewedItem d-flex flex-column col-10 col-sm-5 col-md-3 col-lg-2 mx-3 mt-3 pt-3 bg-light' data-viewed-item='"+element.Title+"'></div>")
-                            .append(anchorImg, anchorTitle, author, bookType, price);
+          $("#bookslib").append(parentDiv);
 
-            $("#bookslib").append(parentDiv);
-
-        })
-      });
+      })
 
       if(hashVal != "")
         $("select#genreSel").val(hashVal).trigger( "change" );
+  }
 
-  })
+  ajaxWorker.postMessage("fetch");
 
 
-
+  // Change event handler
   $('select').on('change', function () {
     var value = $(this).val();
     $("#bkgenre").html(value);
@@ -86,7 +88,38 @@ $(function(){
 
   })
 
+  // Fills book page based on query value
+  var query = window.location.search.substring(7);
+  var qs = decodeURI(query.replace(/%20/g, " ").trim());
+  var json = JSON.parse(sessionStorage.getItem("json"));
+  var titleVar = window.location.search.substring(1,6);
 
+  if(titleVar === "title"){
+    $.each(json.Books, function (index, element) {
+      if(qs === element.Title){
+        var img = $("<img />").height("340")
+                              .attr("src", "images/bookcover/"+element.img);
+        var author = $("<small />").text(" by "+element.Author);
+        var strong = $("<strong />");
+        var title = $("<p></p>");
+        var span = $("<span />");
+        var review = $("<p></p>").text(element.Review)
+                                 .prepend(strong.text("Review: "));
+        var isbn = $("<p class='txtg'></p>").text("ISBN: "+element.ISBN);
+        var price = $("<p class='txtg text-danger'></p>").append("CDN$ "+element.Price);
+        var addToCart = $("<button class='btn btn-warning' data-book='"+element.Title+"' id='cartBtn'></button>").text(" Add to cart")
+        .prepend($("<i class='fa fa-shopping-cart' style='font-size: 18px'></i>"));
+        var bookType = $("<div class='btn border border-warning mb-1 font-weight-bold'></div>").append(element["Book Type"],$("<br />"),span.text("CDN$ "+element.Price).addClass("text-danger"));
+        title.append($("<h4 class='mb-0'></h4>").text(element.Title),author);
+        $("#bkimg").append(img,isbn,price);
+        $("#bkinfo").append(title,bookType,review,addToCart);
+        $("#bkdesc").append();
+        $("a#genrelink").html(element.Genre)
+                        .attr("href", "./books#"+element.Genre);
+        $("#activebook").html(qs);
+      }
+    })
+  }
 
 
 
@@ -203,144 +236,103 @@ $(function(){
       }
    }
 
+   // Shopping Cart
+   var subtotal = 0;
+   shoppingCrtWorker.onmessage = function(event){
+     var small = $("<small />").append(" by " + event.data.author);
+     var title = $("<p class='txtg'></p>").append($("<strong />").text(event.data.title), small);
+	   var language = $("<p class='txtg'></p>").append(event.data.language);
+     var bookType = $("<p class='txtg'></p>").append(event.data.bookType);
+     var price = $("<p class='txtg text-danger'></p>").append("CDN$ "+event.data.price);
+ 		 var quantity = $("<p class='txtg'></p>").append("Qty: 1");
+ 		 var img = $("<img />").attr("src","images/bookcover/"+event.data.img)
+ 							  .attr("clsss","").width("100");
+ 		 var deleteBtn = $("<button class='btn btn-danger ml-auto mt-auto' id='deleItem' data-book-isbn='"+event.data.key+"'></button>").text("delete");
+     var imgDiv = $("<div class=' col-12 col-sm-3 col-md-3 col-lg-2 mr-0'></div>").append(img);
+		 var infoDiv = $("<div class='d-flex flex-column col-12 col-sm-9 col-md-9 col-lg-10'></div>").append(title,bookType,language,price,quantity,deleteBtn);
+  	 var row = $("<div class='row'></div>").append(imgDiv, infoDiv);
+  	 var parentDiv = $("<div class='card col-md-12 pt-3 pb-3 mb-3' data-id='"+event.data.key+"'></div>").append(row);
+		 subtotal += parseFloat(event.data.price);
+		 $("#spcrt").append(parentDiv);
+		 $("span.subtl").html(subtotal.toFixed(2));
+   }
+   shoppingCrtWorker.postMessage("fetch");
 
- // IndexedDB
-  if (!window.indexedDB) {
-    window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
-  }
+   $(".hide-dialog-txt").hide();
 
-   // Open indexedDB a database
-  var request = window.indexedDB.open("Library");
-  var db;
+   // Remove item from carta
+   $("div").delegate("button#deleItem","click",function(){
+ 	  $(".hide-dialog-txt").show();
+ 	  var item = $(this);
+ 	  // Confirmation dialog box
+ 	  $( "#dialog-confirm" ).dialog({
+ 		  resizable: false,
+ 		  height: "auto",
+ 		  width: 400,
+ 		  modal: true,
+ 		  buttons: {
+ 			"Delete item": function() {
+ 				deleteItem(item);
+ 			  $( this ).dialog( "close" );
+ 			},
+ 			Cancel: function() {
+ 			  $( this ).dialog( "close" );
+ 			}
+ 		  }
+ 		});
+   })
 
-  request.onsuccess = function(event) {
-     console.log("DB request successful");
-     db = event.target.result;
-     	var objectStore = db.transaction("Books").objectStore("Books");
-     	var countRequest = objectStore.count();
-      countRequest.onsuccess = function(){
-   		   $("i.itemCnt,span.itemCnt").html(countRequest.result);
-   	  }
-      getBooksViewed();
-  }
-
-  request.onerror = function(event) {
-	// Generic error handler for all errors targeted at this database's
-	// requests!
-	alert("Database error: " + event.target.errorCode);
-  };
-
-  // This event is only implemented in recent browsers
-  request.onupgradeneeded = function(event) {
-    // Save the IDBDatabase interface
-    var db = event.target.result;
-
-    // Create an objectStore for this database
-    var objectStore = db.createObjectStore("Books", { keyPath: "ISBN"});
-    // Create an objectStore for this database
-    var booksViewedObjectStore = db.createObjectStore("BooksViewed", { keyPath: "ISBN"});
-  }
+   function deleteItem(e){
+ 	var item = e.attr("data-book-isbn");
+ 	var request = db.transaction("Books", "readwrite")
+ 		.objectStore("Books")
+ 		.delete(item);
+ 	request.onsuccess = function(event) {
+ 		// It's gone!
+ 		var cartVal = $(".itemCnt").html();
+ 	    $(".itemCnt").html(parseInt(cartVal) - 1);
+ 		$("div[data-id='"+item+"']").remove();
+ 	};
+   }
 
 
   // Adds item to Cart on click
   $("div#bkinfo").delegate("button#cartBtn","click",function(event){
-	  let json = JSON.parse(sessionStorage.getItem("json"));
     var item = $(this).attr("data-book");
-    var cartData;
 
-    $.each(json.Books, function (index, element) {
-      if(item == element.Title){
-        cartData = {
-          ISBN: element.ISBN,
-          title: element.Title,
-          author: element.Author,
-    		  language: element.Language,
-    		  price: element.Price,
-          bookType: element["Book Type"],
-          img: element.img
-        }
+    cartWorker.onmessage = function(e){
+
+      if(!Number.isNaN(parseInt(e.data))){
+        var cartVal = $(".itemCnt").html();
+        $(".itemCnt").html(parseInt(cartVal) + 1);
+      }else {
+        console.log(e.data);
       }
-    })
+    }
 
-	  // open a read/write db transaction, ready to add data
-	  var transaction = db.transaction(["Books"], "readwrite");
-
-	  // report on the success of opening the transaction
-	  transaction.oncomplete = function(event) {
-  		//note.innerHTML += '<li>Transaction completed: database modification finished.</li>';
-      var cartVal = $(".itemCnt").html();
-  		$(".itemCnt").html(parseInt(cartVal) + 1);
-  		console.log("Transaction completed: database modification finished.");
-	  };
-
-	  transaction.onabort = function(event){
-		  console.log("Transaction aborted");
-	  }
-
-	  transaction.onerror = function(event) {
-  	  //note.innerHTML += '<li>Transaction not opened due to error. Duplicate items not allowed.</li>';
-      alert("Item already added to cart");
-  		console.log("Transaction not opened due to error. Duplicate items not allowed");
-	  };
-
-	  // create an object store on the transaction
-	  var objectStore = transaction.objectStore("Books");
-
-	  // add our newItem object to the object store
-	  var objectStoreRequest = objectStore.add(cartData);
-
-	  objectStoreRequest.onsuccess = function(event) {
-  		// report the success of the request (this does not mean the item
-  		// has been stored successfully in the DB - for that you need transaction.oncomplete)
-  		//note.innerHTML += '<li>Request successful.</li>';
-      console.log("Request successful.");
-
-  		//$(".cart").tooltip('enable').tooltip('open');
-	  };
-
-	  // handles errors that may arise
-	  objectStoreRequest.onerror = function(){
-		  console.log("adding to object store failed");
-	  }
+    cartWorker.postMessage({Title: item, Json: JSON.stringify(jsonObj)});
   })
 
+  // Add viewed item on click
+  $("div#bookslib").delegate("div.viewedItem","click",function(){
+      var item = $(this).attr("data-viewed-item");
+      //let jsonObj = JSON.parse(sessionStorage.getItem("json"));
+      viewedWorker.onmessage = function(e){
+        console.log(e.data);
+      }
+
+      viewedWorker.postMessage({Title: item.toString(), Json: JSON.stringify(jsonObj)});
+
+  })
 
   // Get recently viewed book
-  function getBooksViewed(){
-    var objectStore = db.transaction(["BooksViewed"]).objectStore("BooksViewed");
-    var viewCount = objectStore.count();
-    viewCount.onsuccess = function(){
-      console.log("count request successful: " + viewCount.result);
-      if(viewCount.result > 0){
-        $('#recentViewed').show();
-        objectStore.openCursor().onsuccess = function(event){
-          var cursor = event.target.result;
-          if(cursor){
-            $('#viewed')
-              .trigger('add.owl.carousel', [
-                '<div class="card item mb-3">'+
-                '<div class="card-body">'+
-                '<p class="txtg"><a href="./book?title='+cursor.value.title+'"><img src="images/bookcover/'+cursor.value.img+'" height="248" /><a/></p>'+
-                '<p class="txt"><a href="./book?title='+cursor.value.title+'">'+cursor.value.title+'</a></p>'+
-                '<p class="txtg font-weight-bold">'+cursor.value.author+'</p>'+
-                '<p class="txtg">'+cursor.value.bookType+'</p>'+
-                '<p class="txtg text-danger">CDN$ '+cursor.value.price+'</p>'+
-                '</div>'+
-                '</div>'
-              ])
-              .trigger('refresh.owl.carousel');
-
-            cursor.continue();
-          }
-        }
-      }
-    }
-    viewCount.onerror = function(){
-      console.log("count request unsuccessful");
-    }
-    console.log("get viewed books" + viewCount);
-
+  getViewedWorker.onmessage = function(event){
+    $('#recentViewed').show();
+    $('#viewed')
+      .trigger('add.owl.carousel', [event.data])
+      .trigger('refresh.owl.carousel');
   }
+  getViewedWorker.postMessage("fetch");
 
 
   // disable Cart's tooltip
@@ -416,15 +408,4 @@ $(function(){
         }
     });
 
-
-    // Viewed item event handler
-    $("div#bookslib").delegate("div.viewedItem","click",function(){
-        var item = $(this).attr("data-viewed-item");
-        //let jsonObj = JSON.parse(sessionStorage.getItem("json"));
-        worker.onmessage = function(e){
-          console.log("From worker. Item sent: "+e.data);
-        }
-        worker.postMessage(item);
-
-    })
 })
