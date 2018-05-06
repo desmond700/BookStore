007aca389cba1241da011961f79c3420898bc650
desmond700 <dwdeveloper4@gmail.com>
2018-05-05 21:16:11 -0400

    Removed unnecessary code

--- js/addViewedItem_WW.js
+++ js/addViewedItem_WW.js
@@ -1,37 +1,31 @@
 // javaScript Document
 
-var IDBTransaction = IDBTransaction || webkitIDBTransaction;
 
-// Open or, if don't exist, Create IndexedDB
+// Worker response
+self.onmessage = function(e){
+  var title = e.data.Title;
+  var json = JSON.parse(e.data.Json);
+  storeViewedItem(title, json)
+}
+
+// Open indexedDB a database
 var request = indexedDB.open("Library");
 var db;
 
 request.onsuccess = function(event) {
-   db = event.target.result;
+  db = event.target.result;
 }
 
 // This event is only implemented in recent browsers
 request.onupgradeneeded = function(event) {
-  // Save the IDBDatabase interface
-  var db = event.target.result;
-  // Create an objectStore for this database
-  var objectStore = db.createObjectStore("Books", { keyPath: "ISBN"});
-  // Create an objectStore for this database
-  var booksViewedObjectStore = db.createObjectStore("BooksViewed", { keyPath: "ISBN"});
+ // Save the IDBDatabase interface
+ var db = event.target.result;
+
+ // Create an objectStore for this database
+ var objectStore = db.createObjectStore("Books", { keyPath: "ISBN"});
+ // Create an objectStore for this database
+ var booksViewedObjectStore = db.createObjectStore("BooksViewed", { keyPath: "ISBN"});
 }
-
-
-self.onmessage = function(e){
-  var item = e.data;
-
-  fetch("../data/books.json")
-  .then(response => {
-    return response.json();
-  }).then(json => {
-    storeViewedItem(item, json)
-  })
-}
-
 
 function storeViewedItem(item, json){
   var viewedData;
